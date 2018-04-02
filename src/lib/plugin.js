a5d9ce685c6ac7580de2f48107b03c6e48ba800b
ldwqh0 <ldwqh0@outlook.com>
2018-04-03 00:11:53 +0800

    完成loader

--- src/lib/plugin.js
+++ src/lib/plugin.js
@@ -1,9 +1,17 @@
-import VMap from '../components/VMap'
+import config from './config'
 import ScriptLoader from './script-loader'
+import VMap from '../components/VMap'
 
 export default {
   install (Vue, option) {
-    Vue.component('VMap', VMap)
+    Vue.component(VMap.name, VMap)
     Vue.esri = ScriptLoader
+    Vue.prototype.$esri = ScriptLoader
+    if (option.url) {
+      config.url = option.url
+    }
+    if (option.dojoConfig) {
+      window['dojoConfig'] = option.dojoConfig
+    }
   }
 }
--- src/lib/script-loader.js
+++ src/lib/script-loader.js
@@ -1,20 +1,56 @@
-// import option from './option'
+import config from './config'
 
-function createScript (url) {
+const queue = [] // 定义一个操作队列，用于储存script没有完成加载时的任务
+
+const createScript = (url) => {
   const script = document.createElement('script')
   script.type = 'text/javascript'
   script.src = url
   return script
 }
 
+const scriptExist = function () {
+  return !(document.querySelector('script[data-esri-loader]') === null)
+}
+
+const loadScript = () => {
+  if (scriptExist()) {
+    return
+  }
+
+  let script = createScript(config.url)
+  script.setAttribute('data-esri-loader', 'loading')
+  document.body.appendChild(script)
+  const onScriptLoad = () => {
+    script.removeEventListener('load', onScriptLoad)
+    script.setAttribute('data-esri-loader', 'loaded')
+    // 获取队列里面的任务，依次执行
+    while (queue.length > 0) {
+      let fn = queue.shift()
+      fn()
+    }
+  }
+  script.addEventListener('load', onScriptLoad)
+}
+
 export default {
-  loadModules () {
+  loadModules (modules) {
     return new Promise((resolve, reject) => {
-      let script = createScript('https://js.arcgis.com/4.6/')
-      document.body.appendChild(script)
-      script.addEventListener('load', () => {
-        resolve(window['require'])
-      })
+      const resolveModules = modules => {
+        const globalRequire = window['require']
+        globalRequire(modules, (...args) => {
+          resolve(args)
+        })
+      }
+      if (window['require']) {
+        resolveModules(modules)
+      } else {
+        let ops = () => {
+          resolveModules(modules)
+        }
+        queue.push(ops)
+        loadScript()
+      }
     })
   }
 }
